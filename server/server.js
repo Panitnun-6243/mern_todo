96d08e81f2d3d6771b51363b61e22ab55505a654
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-27 02:43:35 +0700

    Add the routes endpoints to the middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8000
+//import endpoint to server.js by create to object and use it
+const todo = require("./routes/todo")
 //import connectDB function from config.js
 require("dotenv").config()
 const connectDB = require("./config/config")
@@ -11,6 +13,9 @@ app.use(express.json({ extended: false}))
 app.get("/", (req, res)=>{
     res.send("Server up and running")
 })
+
+//ใช้งาน routes
+app.use("/api/todo", todo)
 
 app.listen(PORT, ()=>{
     console.log(`server is running on http://localhost:${PORT}`)
