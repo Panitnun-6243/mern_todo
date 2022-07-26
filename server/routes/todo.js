be2a36b764133f3f9ad60b63118f940f8b26bd7e
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-27 02:37:40 +0700

    Add method to end point

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,17 +1,19 @@
 const express = require("express")
 //initialize router
 const router = express.Router()
+//import all CRUD method from controller to endpoint (route)
+const {getAllTodo, postCreateTodo, putUpdateTodo, deleteTodo} = require("../controllers/todo")
 
 //define GET to read all todo
-router.get("/")
+router.get("/", getAllTodo)
 
 //define POST to create new todo
-router.post("/")
+router.post("/", postCreateTodo)
 
 //define PUT to update todo
-router.put("/:id")
+router.put("/:id", putUpdateTodo)
 
 //define DELETE to delete todo
-router.delete("/:id")
+router.delete("/:id", deleteTodo)
 
 module.exports = router
