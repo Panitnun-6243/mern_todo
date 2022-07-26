0a34fa050d5b894253b81868af22624ee55a0223
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-27 03:06:26 +0700

    Add CORS

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8000
+//import cors ไว้ใช้อนุญาติการเรียกใช้ api จาก client (react)
+const cors = require("cors")
+
 //import endpoint to server.js by create to object and use it
 const todo = require("./routes/todo")
+
 //import connectDB function from config.js
 require("dotenv").config()
 const connectDB = require("./config/config")
 connectDB()
+
+//ใช้งาน cors
+app.use(cors({origin: true, credentials: true}))
 
 //สร้าง middileware
 app.use(express.json({ extended: false}))
